bd529453a773ebdf2c07df3f90a6994b7e8b054c
Subin Siby <subins2000@gmail.com>
2016-11-21 08:10:36 +0530

    Added footer

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div id="comments">
+  <p>Created by <a href="http://subinsb.com">Subin Siby</a>.</p>
+  <p>Designs By <a href="https://www.facebook.com/vikaskkunjumon">Vikas</a>, <a href="https://www.facebook.com/maneesh.redline">Maneesh</a></p>
+</div>
